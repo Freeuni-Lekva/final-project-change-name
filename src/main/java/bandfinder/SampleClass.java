ad6220fa7baccb2bceda688e321c797f61711672
sasagh <saba.saghinadze@blocshop.io>
2022-07-10 00:36:56 +0400

    Project initialization

---
+++ src/main/java/bandfinder/SampleClass.java
@@ -0,0 +1,7 @@
+package bandfinder;
+
+public class SampleClass {
+    public static void main(String[] args) {
+        System.out.println("Sample java class");
+    }
+}
---
+++ src/test/java/bandfinder/SampleClassTest.java
@@ -0,0 +1,12 @@
+package bandfinder;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleClassTest {
+    @Test
+    public void testSample() {
+        Assertions.assertEquals(1, 1);
+    }
+}
