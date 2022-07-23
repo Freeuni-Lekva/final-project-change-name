67b979abd7bd333b7fc9ff9bbac59d742878a31d
R4TI <rmena20@freeuni.edu.ge>
2022-07-23 19:39:22 +0400

    resolved injectable

--- src/main/java/bandfinder/infrastructure/AutoInjectable.java
+++ src/main/java/bandfinder/infrastructure/AutoInjectable.java
@@ -6,6 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD,ElementType.LOCAL_VARIABLE})
+@Target(ElementType.FIELD)
 
 public @interface AutoInjectable { }
