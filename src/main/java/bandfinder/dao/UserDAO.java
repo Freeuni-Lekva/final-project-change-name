43ab860ddbde80c86a2c6bc5b42d291be3f93a42
Giorgi Megrelidze <gmegr20@freeuni.edu.ge>
2022-08-10 16:31:30 +0400

    Added search method

--- src/main/java/bandfinder/dao/UserDAO.java
+++ src/main/java/bandfinder/dao/UserDAO.java
@@ -1,10 +1,13 @@
 package bandfinder.dao;
 
 import bandfinder.models.User;
+import java.util.List;
 
 public interface UserDAO extends DAO<User>{
     String ATTRIBUTE_NAME = "UserDAO";
 
     // Debatable
     User getUserByEmail(String email);
+
+    List<User> searchUsersByName(String query);
 }
