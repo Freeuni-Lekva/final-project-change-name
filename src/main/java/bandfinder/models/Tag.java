9f87cb6715a770310d1df246b0ca927646a218fa
Giorgi Megrelidze <gmegr20@freeuni.edu.ge>
2022-08-14 20:57:28 +0400

    Added equals()

--- src/main/java/bandfinder/models/Tag.java
+++ src/main/java/bandfinder/models/Tag.java
@@ -28,4 +28,14 @@ public class Tag {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Tag tag = (Tag) o;
+
+        return id == tag.id;
+    }
 }
