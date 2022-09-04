23f43db0b655a35176b46cb94df211f300609dec
Nika Shalikashvili <nshal20@freeuni.edu.ge>
2022-09-04 13:08:12 +0400

    create post dao interface

--- src/main/java/bandfinder/dao/PostDAO.java
+++ src/main/java/bandfinder/dao/PostDAO.java
@@ -1,4 +1,14 @@
 package bandfinder.dao;
 
-public interface PostDAO {
+import bandfinder.models.Post;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public interface PostDAO extends DAO<Post> {
+    String ATTRIBUTE = "PostDAO";
+
+    List<Post> getUserPosts(int userId);
+
+    List<Post> getUserPostsByDate(int userId, Timestamp startDate, Timestamp endDate);
 }
