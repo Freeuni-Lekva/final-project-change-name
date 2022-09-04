3ad5506a8667313fcc1a98ee1f78721df9bd683c
Giorgi Megrelidze <gmegr20@freeuni.edu.ge>
2022-09-04 16:13:02 +0400

    Temporarily removed malfunctioning DB migrator

--- src/main/java/bandfinder/listeners/InitializationListener.java
+++ src/main/java/bandfinder/listeners/InitializationListener.java
@@ -1,13 +1,6 @@
 package bandfinder.listeners;
 
-import bandfinder.infrastructure.DatabaseMigrator;
-import bandfinder.serviceimplementations.HashMapUserDAO;
-import bandfinder.dao.UserDAO;
 import bandfinder.infrastructure.Config;
-import bandfinder.models.User;
-import bandfinder.serviceimplementations.HashingServiceImpl;
-import bandfinder.services.HashingService;
-
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -17,47 +10,34 @@ public class InitializationListener implements
         ServletContextListener,
         HttpSessionListener,
         HttpSessionAttributeListener {
-    public InitializationListener() {
-    }
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        DatabaseMigrator.migrate();
-
-        UserDAO currentUserDAO = new HashMapUserDAO();
-        sce.getServletContext().setAttribute(UserDAO.ATTRIBUTE_NAME, currentUserDAO);
-        currentUserDAO.create(new User(1, "guja@gmail.com", "pass", "gujaboi", "sth", "none"));
-
+//        DatabaseMigrator.migrate(); Temporarily disabled, - changing delimiters not supported
         Config.ConfigureServices();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        /* This method is called when the servlet Context is undeployed or Application Server shuts down. */
+    }
+
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent event) {
+    }
+
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent event) {
+    }
+
+    @Override
+    public void attributeReplaced(HttpSessionBindingEvent event) {
     }
 
     @Override
     public void sessionCreated(HttpSessionEvent se) {
-        /* Session is created. */
     }
 
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
-        /* Session is destroyed. */
-    }
-
-    @Override
-    public void attributeAdded(HttpSessionBindingEvent sbe) {
-        /* This method is called when an attribute is added to a session. */
-    }
-
-    @Override
-    public void attributeRemoved(HttpSessionBindingEvent sbe) {
-        /* This method is called when an attribute is removed from a session. */
-    }
-
-    @Override
-    public void attributeReplaced(HttpSessionBindingEvent sbe) {
-        /* This method is called when an attribute is replaced in a session. */
     }
 }
