420356fe0a0b2718717908b7b1c2130fa8acca9e
Nika Shalikashvili <nshal20@freeuni.edu.ge>
2022-09-05 15:54:52 +0400

    add post dao to config

--- src/main/java/bandfinder/infrastructure/Config.java
+++ src/main/java/bandfinder/infrastructure/Config.java
@@ -1,14 +1,12 @@
 package bandfinder.infrastructure;
 
-import bandfinder.dao.BandDAO;
-import bandfinder.dao.FollowDAO;
+import bandfinder.dao.*;
+import bandfinder.models.Post;
 import bandfinder.serviceimplementations.*;
 import bandfinder.services.HashingService;
 import bandfinder.services.SearchService;
 import org.apache.ibatis.jdbc.SQL;
-import bandfinder.dao.UserDAO;
 import bandfinder.dao.BandDAO;
-import bandfinder.dao.TagDAO;
 
 public class Config {
     private static boolean isConfigured = false;
@@ -22,6 +20,7 @@ public class Config {
         Injector.injectSingleton(BandDAO.class, SQLBandDAO.class);
         Injector.injectSingleton(FollowDAO.class, SQLFollowDAO.class);
         Injector.injectSingleton(TagDAO.class, SQLTagDAO.class);
+        Injector.injectSingleton(PostDAO.class, SQLPostDAO.class);
         isConfigured = true;
     }
 }
