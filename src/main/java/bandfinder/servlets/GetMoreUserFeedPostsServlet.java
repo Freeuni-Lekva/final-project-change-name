3bb357fb28588f094de2b9b29ec9997e43dc49b2
Nika Shalikashvili <nshal20@freeuni.edu.ge>
2022-09-07 17:43:51 +0400

    add .js file for user feed

--- src/main/java/bandfinder/servlets/GetMoreUserFeedPostsServlet.java
+++ src/main/java/bandfinder/servlets/GetMoreUserFeedPostsServlet.java
@@ -1,15 +1,23 @@
 package bandfinder.servlets;
 
+import bandfinder.dao.PostDAO;
+import bandfinder.infrastructure.AutoInjectable;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "Servlet", value = "/Servlet")
-public class GetMoreUserFeedPosts extends ServletBase {
+public class GetMoreUserFeedPostsServlet extends ServletBase {
+
+    @AutoInjectable
+    private PostDAO postDAO;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        PrintWriter out = response.getWriter();
     }
 
     @Override
